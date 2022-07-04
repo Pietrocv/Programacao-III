589ca5b07f0b029b77cdbdcbe8adf521227a0bf8
Pietro Visentin <86116655+Pietrocv@users.noreply.github.com>
2022-07-04 16:35:37 -0300

    Add files via upload

---
+++ PR3/js/cadastro.js
@@ -0,0 +1,9 @@
+function showPassword (senha = 'senha'){
+    let inputSenha = document.querySelector(`#${senha}`)
+
+    if(inputSenha.getAttribute('type') === 'password'){
+        inputSenha.setAttribute('type', 'text')
+    }else{
+        inputSenha.setAttribute('type', 'password')
+    }
+}
